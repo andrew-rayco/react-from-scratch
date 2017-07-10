8e5f455d926d92cdd13762b7cb98247150f06710
Andrew <andy@andycondon.com>
2017-07-10 20:19:26 +1200

    setting state

--- client/App.js
+++ client/App.js
@@ -4,20 +4,50 @@ import ReactDOM from 'react-dom'
 class App extends React.Component {
   constructor (props) {
     super(props),
-    this.state = { hat: 'bowler' }
+    this.state = {
+      hat: '',
+      count: 0
+     }
+  }
+
+  componentWillMount () {
+    this.randomHat()
+  }
+
+  upCount () {
+    this.setState((prevState) => {
+      return {count: prevState.count + 1}
+      })
+    console.log(this.state.count)
+  }
+
+  randomHat () {
+    var hats = ['bowler', 'top', 'rasta', 'baseball', 'dunce', 'party']
+    var randomNum = Math.floor(Math.random() * hats.length)
+    this.setState(() => {
+      return {
+        hat: hats[randomNum]
+      }
+    })
   }
 
   render() {
     return (
-      <div>My hat is a {this.state.hat}</div>
+      <div>
+        <h1>My hat is a {this.state.hat} hat. I've worn it {this.state.count} times</h1>
+        <button onClick={() => this.upCount()}>Wear hat</button>
+        <button onClick={() => this.randomHat()}>Change hat</button>
+      </div>
+
     )
   }
 
 }
 
 function test (props) {
+  {console.log('test', props)}
   return (
-    <h1>This is a test</h1>
+    <h1>This is a test.</h1>
   )
 }
 
